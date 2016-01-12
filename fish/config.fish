4c9090411e296282e33adb81423e7da53b91895a
Adam Lloyd <adam@alloy-d.net>
2016-01-12 16:47:17 -0500

    Fish: add OS X system Ruby's user-local gem bin path to PATH.

--- fish/config.fish
+++ fish/config.fish
@@ -30,7 +30,7 @@ alias gst='git st'
 # -----------
 
 set -x GOPATH $HOME/.go
-set -x PATH $HOME/.rbenv/shims $GOPATH/bin /usr/local/opt/go/libexec/bin $PATH
+set -x PATH $HOME/.rbenv/shims $HOME/.gem/ruby/2.0.0/bin $GOPATH/bin /usr/local/opt/go/libexec/bin $PATH
 
 set -x COWPATH "$HOME/.cows:$COWPATH"
 
