7a5e43e4d4d2bc70cf869f00b9c7461d09cff09a
Adam Lloyd <adam@alloy-d.net>
2020-08-04 07:36:02 -0400

    fish: add env variables to support running Wayland.

--- .config/fish/os-specific/Linux.fish
+++ .config/fish/os-specific/Linux.fish
@@ -1,6 +1,9 @@
 set -x AUR_PAGER /usr/bin/ranger
-set -x SXHKD_SHELL /usr/bin/dash
 
 set -x DOWNLOADS "$HOME/downloads"
 set -x XDG_DOWNLOAD_DIR "$DOWNLOADS"
 
+set -x QT_QPA_PLATFORM "wayland"
+set -x XKB_DEFAULT_VARIANT "colemak"
+
+set -x SXHKD_SHELL /usr/bin/dash
