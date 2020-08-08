e6b7ab0ff96e5c9ba41208d79480ba162535c7e7
Adam Lloyd <adam@alloy-d.net>
2020-08-08 10:28:36 -0400

    sway: clean up config, especially workspace name stuff.

---
+++ .config/sway/workspace-number-to-name.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/dash
+
+NUM="$1"
+INDEX=`expr $NUM - 1`
+
+# waybar sorts workspace names, swaybar does not.
+SORT="true"
+
+getworkspaces() {
+  swaymsg -r -t get_workspaces
+}
+
+if test "$SORT" = "true"; then
+  getworkspaces | jq -r "map(.name)|sort|.[$INDEX] // $NUM"
+else
+  getworkspaces | jq -r ".[$INDEX].name // $NUM"
+fi
