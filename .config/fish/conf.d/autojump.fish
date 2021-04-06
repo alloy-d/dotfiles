b630274dd57714c6753cd527653ed990f741e6c4
Adam Lloyd <adam@alloy-d.net>
2021-04-06 08:25:22 -0400

    fish: also load autojump from ARM homebrew dir.

--- .config/fish/conf.d/autojump.fish
+++ .config/fish/conf.d/autojump.fish
@@ -1,6 +1,8 @@
 if status --is-interactive
   if [ -f /usr/share/autojump/autojump.fish ]
     source /usr/share/autojump/autojump.fish
+  else if [ -f /opt/homebrew/share/autojump/autojump.fish ]
+    source /opt/homebrew/share/autojump/autojump.fish
   else if [ -f /usr/local/share/autojump/autojump.fish ]
     source /usr/local/share/autojump/autojump.fish
   end
