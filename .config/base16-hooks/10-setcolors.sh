ff65cc68383ab7eb981b11c8056842ea56131100
Adam Lloyd <adam@alloy-d.net>
2021-12-07 15:37:38 -0500

    base16: there's just no reason to do anything to kitty.

--- .config/base16-hooks/10-setcolors.sh
+++ .config/base16-hooks/10-setcolors.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 BASE16="${HOME}/.local/share/base16"
-BASE16_KITTY="${BASE16}/kitty"
 BASE16_ROFI="${BASE16}/rofi"
 BASE16_SWAY="${BASE16}/sway"
 BASE16_XRESOURCES="${BASE16}/xresources"
@@ -50,32 +49,6 @@ configuration {
 EOF
 }
 
-set_kitty() {
-  echo "Configuring kitty..."
-  source="${BASE16_KITTY}/colors/${THEME}-256.conf"
-  config="${HOME}/.config/kitty/kitty.conf"
-  colors_config="$(mktemp)"
-
-  if [ ! -f "$source" ]; then
-    echo "Problem setting kitty theme: '${source}' doesn't exist." >&2
-    return 2
-  fi
-
-  # Generate a new config with just the colors stuff.
-  echo "include ${source}" >> "${colors_config}"
-  grep 'color9 ' < "${source}" | sed 's/color9/cursor/' >> "${colors_config}"
-
-  # Get rid of the old colors stuff.
-  sed -i "\|^include ${BASE16}|d" "${config}"
-  sed -i '/^cursor /d' "${config}"
-
-  # Add the new colors config to the config file.
-  cat < "${colors_config}" >> "${config}"
-
-  # Set colors now in currently running kitties.
-  pgrep -x kitty && kitty @ set-colors -a "${colors_config}" >/dev/null 2>&1 || true
-}
-
 reload_i3() {
   echo "Reloading i3..."
   # Colors come from Xresources, so just need to reload.
@@ -91,7 +64,6 @@ fi
 THEME=$(basename "$(readlink "$SHELL_THEME_LINK")" .sh)
 echo "Setting theme ${THEME}..."
 
-set_kitty
 set_rofi
 set_sway
 set_xresources
