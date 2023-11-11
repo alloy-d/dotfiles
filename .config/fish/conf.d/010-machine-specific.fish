a95095f9d94dba1a6b4a284583cc0dd61cc94ed9
Adam Lloyd <adam@alloy-d.net>
2023-11-11 09:44:10 -0500

    fish: oopsie, gotta put brew stuff on PATH earlier.

--- .config/fish/conf.d/010-machine-specific.fish
+++ .config/fish/conf.d/010-machine-specific.fish
@@ -1,3 +1,7 @@
+# This file needs to run before most other configuration, because (at
+# least for macOS) it can alter PATH to include things that other
+# files need.
+
 set -l os_specific "$__fish_config_dir/os-specific/$(uname).fish"
 set -l host_specific "$__fish_config_dir/host-specific/$(hostname -s).fish"
 
