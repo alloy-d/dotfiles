da00c70c120a8d0b5c4c28f7782039aa8b9c206a
Adam Lloyd <adam@alloy-d.net>
2026-02-23 11:03:49 -0500

    fish: tweak jj aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,8 +27,6 @@ if status --is-interactive
   # jj - command abbreviations, only available in fish >= 4
   if test (string sub -l 1 $version) -ge 4
     abbr -a -c jj np 'new -B @'                       # "new parent"
-    abbr -a -c jj sp 'squash --interactive --into @-' # "squash into parent"
-
     abbr -a -c jj nne 'new --no-edit'
     abbr -a -c jj nnep 'new --no-edit -B @'
     abbr -a -c jj nnea 'new --no-edit --after'
@@ -36,9 +34,11 @@ if status --is-interactive
 
     abbr -a -c jj si 'squash --interactive'
     abbr -a -c jj sit 'squash --interactive --to'
+    abbr -a -c jj sp 'squash --interactive --into @-' # "squash into parent"
 
     abbr -a -c jj bmt 'bookmark move trunk --to @'
     abbr -a -c jj bmtt 'bookmark move trunk --to'
+    abbr -a -c jj bmtp 'bookmark move trunk --to @-'
 
     abbr -a -c jj gf 'git fetch'
     abbr -a -c jj gp 'git push'
